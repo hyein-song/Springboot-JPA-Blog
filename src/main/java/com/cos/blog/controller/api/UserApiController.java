b581ea4bf8ca6a705e510fcac42e4e13bedb3538
hyein-song <shi9512@naver.com>
2021-11-19 18:54:43 +0900

    security 설정

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -18,23 +18,6 @@ public class UserApiController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private HttpSession session;
-
-    @PostMapping("/api/user/login")
-    public ResponseDto<Integer> login(@RequestBody User user){ // spring에서는 Session을 매개변수로 받을 수 있다. // session을 매개변수로 받지 않고 변수로 할 수도 있다.
-        System.out.println("UserApiController : login 호출됨");
-        User principal = userService.로그인(user); // principal : 접근 주체
-
-        if(principal != null) {
-            session.setAttribute("principal",principal);
-        }
-
-        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
-
-    }
-
-
     @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user){ // json이니까 RequestBody 써주기
         System.out.println("UserApiController : save 호출됨");
@@ -43,5 +26,22 @@ public class UserApiController {
         int result = userService.회원가입(user);
         return new ResponseDto<Integer>(HttpStatus.OK.value(),result);
     }
+
+
+//    @Autowired
+//    private HttpSession session;
+    // 로그인은 security를 이용해 구현
+//    @PostMapping("/api/user/login")
+//    public ResponseDto<Integer> login(@RequestBody User user){ // spring에서는 Session을 매개변수로 받을 수 있다. // session을 매개변수로 받지 않고 변수로 할 수도 있다.
+//        System.out.println("UserApiController : login 호출됨");
+//        User principal = userService.로그인(user); // principal : 접근 주체
+//
+//        if(principal != null) {
+//            session.setAttribute("principal",principal);
+//        }
+//
+//        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
+//
+//    }
 
 }
