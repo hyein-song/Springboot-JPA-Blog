01916e25f13484628edfcd5072b8a823faa5eb74
hyein-song <shi9512@naver.com>
2021-11-19 21:36:33 +0900

    test시 csrf disable

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.cos.blog.config;
 
+import com.cos.blog.model.User;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 // 빈 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 하는것
 
@@ -13,11 +16,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 체크하겠다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public BCryptPasswordEncoder encoderPW(){
+        return new BCryptPasswordEncoder(); // 빈이 되었으니 스프링이 관리
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
+            .csrf().disable() // csrf 토큰 비활성화 (테스트 시 걸어두는게 좋음)
             .authorizeRequests()
-                .antMatchers("/auth/**")
+                .antMatchers("/","/auth/**","/js/**","/css/**","/image/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
