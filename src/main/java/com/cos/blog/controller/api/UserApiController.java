3af5a3f511311d6b412cede0adcaf73a38b42aa3
hyein-song <shi9512@naver.com>
2021-11-19 21:36:19 +0900

    user 설정 service로 옮김

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -6,6 +6,7 @@ import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +22,6 @@ public class UserApiController {
     @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user){ // json이니까 RequestBody 써주기
         System.out.println("UserApiController : save 호출됨");
-        // 실제로 insert 하고 아래에서 return
-        user.setRole(RoleType.USER);
         int result = userService.회원가입(user);
         return new ResponseDto<Integer>(HttpStatus.OK.value(),result);
     }
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,8 +1,10 @@
 package com.cos.blog.service;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,9 +26,16 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     @Transactional
     public int 회원가입(User user){
         try{
+            String rawPassword = user.getPassword();
+            String encPassword = encoder.encode(rawPassword); // 해시
+            user.setPassword(encPassword);
+            user.setRole(RoleType.USER);
             userRepository.save(user);
             return 1;
         } catch (Exception e){
