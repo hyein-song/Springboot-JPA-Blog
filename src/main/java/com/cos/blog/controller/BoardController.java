ec92cb46f1740053534f91d2110abea7f6f1507d
hyein-song <shi9512@naver.com>
2021-11-21 01:40:02 +0900

    글 목록 페이징

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,16 +1,26 @@
 package com.cos.blog.controller;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping({"","/"})
-    public String index(){ // 컨트롤러에서 세션을 어떻게 찾는지?
-        return "index";
+    public String index(Model model,@PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){ // 데이터를 가져올때 모델이 필요하다
+        model.addAttribute("boards",boardService.글목록(pageable)); // 모델에 정보를 넣으면 아래의 view까지 모델을 끌고 이동을 한다.
+        return "index"; // viewResolver 작동
     }
     
     // USER 권한이 필요
